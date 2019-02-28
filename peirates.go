8bf87008a46a852bc8f09673fba5277331166020
Faith Analog <faith@faithanalog.com>
2019-02-27 19:42:00 -0800

    v1.02

--- peirates.go
+++ peirates.go
@@ -535,7 +535,7 @@ func PeiratesMain() {
 	,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 	________________________________________`)
 
-	println("\n\nPeirates v1.01 by InGuardians")
+	println("\n\nPeirates v1.02 by InGuardians")
 	println("https://www.inguardians.com/labs/\n")
 	parseOptions(&cmdOpts)
 
