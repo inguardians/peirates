b246cce25cbe1bf0cc337f7fec10aa60a3309f90
jay <jay@MacBookPro.local>
2019-03-02 16:15:16 -0800

    v1.0.4 tagging

--- peirates.go
+++ peirates.go
@@ -632,7 +632,7 @@ func banner(connectionString ServerInfo) {
 ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 ________________________________________
 
-   Peirates v1.03 by InGuardians
+   Peirates v1.04 by InGuardians
    https://www.inguardians.com/labs/
 
 ----------------------------------------------------------------`)
