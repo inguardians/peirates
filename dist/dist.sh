6f808274669cc2ddc56bb4d8f24051582127b311
Jay Beale <jay.beale@gmail.com>
2019-12-27 13:37:58 -0800

    Build time optimization

--- dist/dist.sh
+++ dist/dist.sh
@@ -2,7 +2,7 @@
 
 build() {
     echo "$1"
-    GOOS=linux GOARCH="$1" go build ../cmd/peirates
+    GOOS=linux GOARCH="$1" go build -i ../cmd/peirates
     mkdir peirates-linux-"$1"
     mv peirates peirates-linux-"$1"
     tar cJf peirates-linux-"$1".tar.xz peirates-linux-"$1"
@@ -10,7 +10,12 @@ build() {
     rmdir peirates-linux-"$1"
 }
 
-build amd64
-build arm
-build arm64
-build 386
+if [ -z $1 ] ; then
+    build amd64
+    build arm
+    build arm64
+    build 386 
+else
+    build $1
+fi
+
