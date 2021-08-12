94c0746abc67aaa22ffff2d7938680d7eaa7e1e2
Jay Beale <jay.beale@gmail.com>
2021-08-12 12:48:03 -0400

    Added Steve Campbell's CIDR-to-Hosts code

--- portscan.go
+++ portscan.go
@@ -1,7 +1,9 @@
 package peirates
 
 import (
+	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
 	"sort"
 )
@@ -24,39 +26,67 @@ func scan_controller(ip string) {
 	results := make(chan int)
 
 	var openports []int
-	
+
 	// Start up one worker per port?
-	for i := 0 ; i < cap(ports) ; i++ {
+	for i := 0; i < cap(ports); i++ {
 		go scan_worker(ip, ports, results)
 	}
-	
+
 	// Start up a parallel thread to send ports into the channel
 	go func() {
-		for i:= 1 ; i <=65535 ; i++ {
+		for i := 1; i <= 65535; i++ {
 			ports <- i
 		}
 	}()
-	
+
 	// Go get the results, adding them to the openports array/slice
-	for i := 0 ; i < 65535 ; i++ {
+	for i := 0; i < 65535; i++ {
 		port := <-results
 		if port != 0 {
-			openports = append(openports,port)
+			openports = append(openports, port)
 		}
 	}
-	
+
 	// Close the ports worker assignment channel
 	close(ports)
 	// Close the results channel
 	close(results)
-	
+
 	// Sort the set of openports in place.
 	sort.Ints(openports)
 	for _, port := range openports {
-		fmt.Printf("%d/tcp open\n", port) 
+		fmt.Printf("%d/tcp open\n", port)
 	}
 }
 
+// This function included with permission of the author.
+// See his blog post here:
+// https://www.stevencampbell.info/Golang-Convert-CIDR-Address-To-Hosts/
+func cidrHosts(network string) []string {
+	// convert string to IPNet struct
+	_, ipv4Net, err := net.ParseCIDR(network)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convert IPNet struct mask and address to uint32
+	mask := binary.BigEndian.Uint32(ipv4Net.Mask)
+	// fing the start IP address
+	start := binary.BigEndian.Uint32(ipv4Net.IP)
+	// find the final IP address
+	finish := (start & mask) | (mask ^ 0xffffffff)
+	// make a slice to return host addresses
+	var hosts []string
+	// loop through addresses as uint32
+	for i := start + 1; i <= finish-1; i++ {
+		// convert back to net.IPs
+		ip := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip, i)
+		hosts = append(hosts, ip.String())
+	}
+	// return a slice of strings containing IP addresses
+	return hosts
+}
+
 func main() {
-   scan_controller("192.168.48.1");
- }
+	scan_controller("192.168.48.1/24")
+}
