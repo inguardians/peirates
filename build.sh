985aea918bd2b62a08bd30355a3b58d464082843
Jay Beale <jay.beale@gmail.com>
2021-10-01 15:53:20 -0700

    Updated build.sh to return error code

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-echo "Building..."
+echo "Building for Linux on AMD64..."
 GOOS=linux GOARCH=amd64 go build -v ./cmd/peirates/peirates.go
-echo "Final executable at $PWD/peirates"
-chmod 755 $PWD/peirates
+exit_code=$?
+
+if [ $exit_code -eq 0 ] ; then
+    echo "Final executable at $PWD/peirates"
+    chmod 755 $PWD/peirates
+    exit 0
+else
+    exit $exit_code
+fi
