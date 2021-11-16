6b5fcaac19c8731a69dd496a1cbfd18cfc125a13
Jay Beale <jay.beale@gmail.com>
2021-11-15 21:51:20 -0800

    improved compile-and-package script

--- dist/dist.sh
+++ dist/dist.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-build() {
-    echo "$1"
-    GOOS=linux GOARCH="$1" go build -i ../cmd/peirates
-    mkdir peirates-linux-"$1"
-    mv peirates peirates-linux-"$1"
+compress() {
     tar cJf peirates-linux-"$1".tar.xz peirates-linux-"$1"
     rm peirates-linux-"$1"/peirates
     rmdir peirates-linux-"$1"
 }
 
+build() {
+    echo "$1"
+    arch=$1
+
+    GOOS=linux GOARCH="$arch" go build -i ../cmd/peirates
+    mkdir peirates-linux-"$1"
+    mv peirates peirates-linux-"$1"
+
+    if  [ $COMPRESS == "yes" ] ; then
+       compress $arch
+    fi
+
+}
+
+COMPRESS=yes
+if [ "$2" = "buildonly" ] ; then
+    COMPRESS=no
+fi
+
 if [ -z $1 ] ; then
-    build amd64
-    build arm
-    build arm64
-    build 386 
+    for arch in amd64 arm arm64 386 ; do
+       build $arch
+    done
 else
     build $1
 fi
