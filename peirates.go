160b5e66cbacf72f6b3671c0249e372bdaa295b8
Jay Beale <jay.beale@gmail.com>
2023-05-09 15:50:39 -0700

    updated list of kubelet kubeconfig files and better handle when one isn't formatted correctly

--- peirates.go
+++ peirates.go
@@ -1256,7 +1256,7 @@ func printBanner(interactive bool) {
 ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,`)
 	}
 	println(`________________________________________
-	Peirates v1.1.10 by InGuardians
+	Peirates v1.1.10a by InGuardians
   https://www.inguardians.com/peirates
 ----------------------------------------------------------------`)
 }
