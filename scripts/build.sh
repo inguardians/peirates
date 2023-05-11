0fc54576091a16d9998a9809030cf912d5c65d20
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2023-05-11 11:57:07 -0400

    update the build script with realpath

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,14 +5,14 @@
 # v0.2 - 08 May 2023 - Minor tweaks
 
 echo "Building for Linux on AMD64..."
-GOOS=linux GOARCH=amd64 go build -v -ldflags="-s -w" ../cmd/peirates
+GOOS=linux GOARCH=amd64 go build -v -ldflags="-s -w" $(realpath ../cmd/peirates)
 exit_code=$?
 
 if [ $exit_code -eq 0 ] ; then
   chmod 755 peirates
   mv peirates ..
   echo "Final executable at $PWD/peirates"
-  cat ../peirates | gzip -c >../peirates.gz
+  cat $(realpath ../peirates) | gzip -c > $(realpath ../peirates)
   exit 0
 else
   exit $exit_code
