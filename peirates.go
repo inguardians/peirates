1db1ea2be4fc6c23ae32f09ad2f32c668f09198d
Jay Beale <jay.beale@gmail.com>
2024-04-29 23:38:01 -0700

    peirates.go cleanup

--- peirates.go
+++ peirates.go
@@ -715,49 +715,7 @@ func Main() {
 		// [93] Run a simple all-ports TCP port scan against an IP address [tcpscan]
 		case "93", "tcpscan", "tcp scan", "portscan", "port scan":
 
-			var matched bool
-
-			for !matched {
-				println("Enter an IP address to scan or hit enter to exit the portscan function: ")
-				_, err = fmt.Scan(&input)
-				if err != nil {
-					println("Input error: %v", err)
-					break
-				}
-				if input == "" {
-					break
-				}
-				check_pattern_1, err := regexp.Match(`^\s*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s*$`, []byte(input))
-				if err != nil {
-					println("Error on regexp match against IP address pattern.")
-					continue
-				}
-				if check_pattern_1 {
-					// Scan an IP
-					println("Scanning " + input)
-					scan_controller(input)
-					break
-				} else {
-					check_pattern_2, err := regexp.Match(`^\s*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(\/[0,1,2,3]?\d)\s*$`, []byte(input))
-					if err != nil {
-						println("Error on regexp match against ip/bits CIDR pattern.")
-						continue
-					}
-					if check_pattern_2 {
-						println("Hidden CIDR scan mode used - this may be slow or unpredictable")
-						hostList := cidrHosts(input)
-						for _, host := range hostList {
-							println("Scanning " + host)
-							scan_controller(host)
-						}
-						break
-					} else {
-						println("Error: input must match an IP address or a CIDR formatted network.")
-						continue
-					}
-
-				}
-			}
+			tcpScan(interactive)
 
 		case "94", "enumerate-dns":
 
