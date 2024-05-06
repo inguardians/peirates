b43fb8ad31f3c48153d17df8bb5ac2ad5ec1b309
Jay Beale <jay.beale@gmail.com>
2024-05-05 17:29:03 -0700

    added command-completion to another submenu

--- use-authcani-menu.go
+++ use-authcani-menu.go
@@ -1,9 +1,23 @@
 package peirates
 
 import (
-	"fmt"
+	"io"
 	"strings"
+
+	"github.com/ergochat/readline"
 )
+
+func setUpCompletionAuthCanIMenu() *readline.PrefixCompleter {
+	completer := readline.NewPrefixCompleter(
+		// [true] Set peirates to check whether an action is permitted
+		readline.PcItem("true"),
+		// [false] Set peirates to skip the auth can-i check
+		readline.PcItem("false"),
+		// [exit] Leave the setting at its current value
+		readline.PcItem("exit"),
+	)
+	return completer
+}
 
 func setAuthCanIMenu(UseAuthCanI *bool, interactive bool) {
 
@@ -18,9 +32,41 @@ func setAuthCanIMenu(UseAuthCanI *bool, interactive bool) {
 
 	println("\nChoice: ")
 
+	// Set up main menu tab completion
+	var completer *readline.PrefixCompleter = setUpCompletionAuthCanIMenu()
+
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          "\033[31m»\033[0m ",
+		HistoryFile:     "/tmp/peirates.tmp",
+		AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+
+		HistorySearchFold: true,
+		// FuncFilterInputRune: filterInput,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	// l.CaptureExitSignal()
+
 	var input string
 
-	_, err := fmt.Scanln(&input)
+	line, err := l.Readline()
+	if err == readline.ErrInterrupt {
+		if len(line) == 0 {
+			println("Empty line")
+			pauseToHitEnter(interactive)
+			return
+		}
+	} else if err == io.EOF {
+		println("Empty line")
+		pauseToHitEnter(interactive)
+		return
+	}
+	input = strings.TrimSpace(line)
+
 	if err != nil {
 		println("Error reading input: %v", err)
 		pauseToHitEnter(interactive)
