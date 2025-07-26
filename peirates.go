487a29fe1541fb1a50be47c8e3589948ae291f7a
Jay Beale <jay.beale@gmail.com>
2025-07-25 21:59:30 -0700

    renamed menu item, move to static

--- peirates.go
+++ peirates.go
@@ -27,7 +27,7 @@ var UseAuthCanI bool = true
 // Main starts Peirates[]
 func Main() {
 	// Peirates version string
-	var version = "1.1.28-a"
+	var version = "1.1.28-b"
 
 	var err error
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,8 @@
 
 echo "Building for Linux on AMD64..."
 # For static build, uncomment the below line and comment the one below it.
-GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"  $(realpath ../cmd/peirates)
-#GOOS=linux GOARCH=amd64 go build -tags netgo,osusergo -a --ldflags '-extldflags "-static"' $(realpath ../cmd/peirates)
+#GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"  $(realpath ../cmd/peirates)
+GOOS=linux GOARCH=amd64 go build -tags netgo,osusergo -a --ldflags '-extldflags "-static"' $(realpath ../cmd/peirates)
 
 exit_code=$?
 
